9f231345abd54121a803d7c04e079691cb60e6b2
Prashant <dev.prashant.rajpurohit@gmail.com>
2024-09-28 11:48:14 +0530

    patch put delete

--- src/app.js
+++ src/app.js
@@ -26,6 +26,21 @@ app.get("/user", async (req, res) => {
     res.status(400).send("Something went wrong");
   }
 });
+app.get("/user/:id", async (req, res) => {
+  const userid = req.params.id;
+  try {
+    const user = await User.findById(userid);
+    console.log(user);
+
+    if (!user) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 app.get("/feed", async (req, res) => {
   try {
     const user = await User.find({});
@@ -40,6 +55,38 @@ app.get("/feed", async (req, res) => {
     res.status(400).send("Something went wrong");
   }
 });
+
+//create delete api here on your own
+app.delete("/user/delete/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.send("User Deleted Successfully");
+  } catch (err) {
+    console.log(err);
+  }
+});
+//find the diff btw PATCH and PUT
+app.patch("/user/edit/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      returnDocument: "after", //this will return updated document by default it will return old doc
+    });
+    res.send(user);
+  } catch (err) {}
+});
+app.put("/user/edit/", async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { email: req.body.email },
+      req.body,
+      {
+        returnDocument: "after", //this will return updated document by default it will return old doc
+      }
+    );
+    res.send(user);
+  } catch (err) {}
+});
+
 connectDb()
   .then(() => {
     console.log("CONNECTED to db successfully");
